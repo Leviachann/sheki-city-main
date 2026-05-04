0b320f3588f84f391ad2660e472494d581a8e6c3
Leviachann <amazonsrller@gmail.com>
2026-05-04 15:30:03 +0400

    icons added

--- src/assets/images/icons/arrows.tsx
+++ src/assets/images/icons/arrows.tsx
@@ -1,19 +1,95 @@
 export const ArrowDown = () => {
-    return (
-        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none'
-             stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'
-             className='feather feather-chevron-right'>
-            <polyline points='9 18 15 12 9 6'></polyline>
-        </svg>
-    );
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_179_219)">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M0.263131 4.37931C-0.0876438 4.74373 -0.0876438 5.33371 0.263131 5.6972L7.7 13.4041C8.40244 14.132 9.542 14.132 10.2444 13.4041L17.7367 5.64128C18.0838 5.28059 18.0886 4.69806 17.7459 4.33271C17.396 3.9599 16.8193 3.95525 16.464 4.32247L9.60854 11.4273C9.25687 11.7917 8.68755 11.7917 8.33588 11.4273L1.53491 4.37931C1.18413 4.01488 0.613898 4.01488 0.263131 4.37931Z"
+          fill="#04675C"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_179_219">
+          <rect
+            width="18"
+            height="18"
+            fill="white"
+            transform="translate(18) rotate(90)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
 };
-
+export const ArrowLeft = () => {
+  return (
+    <svg
+      width="35"
+      height="35"
+      viewBox="0 0 44 44"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        opacity="0.5"
+        width="44"
+        height="44"
+        rx="22"
+        fill="url(#paint0_linear_179_206)"
+        fillOpacity="0.7"
+      />
+      <g clipPath="url(#clip0_179_206)">
+        <path
+          d="M15.2247 20.4036L22.2877 12.7041C22.8585 12.0313 22.7758 11.0231 22.103 10.4523C21.4301 9.88152 20.4219 9.96423 19.8511 10.6371L10.4587 20.876C10.3236 21.0117 10.2142 21.1706 10.1356 21.3452C10.0379 21.5623 9.99007 21.7984 9.99554 22.0363L9.99556 22.0378C10.001 22.2752 10.0592 22.5085 10.1658 22.7206C10.1762 22.7412 10.187 22.7615 10.1983 22.7816C10.2634 22.8981 10.3429 23.0058 10.435 23.1024L20.0772 33.5053C20.677 34.1524 21.6879 34.1907 22.335 33.5909C22.9821 32.9911 23.0204 31.9803 22.4206 31.3332L15.2519 23.5989H32.3721C33.2544 23.5989 33.9697 22.8836 33.9697 22.0013C33.9697 21.1189 33.2544 20.4036 32.3721 20.4036H15.2247V20.4036Z"
+          fill="#04675C"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_179_206"
+          x1="-114.663"
+          y1="32"
+          x2="174.653"
+          y2="22.8487"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="white" />
+          <stop offset="1" stopColor="#04675C" stopOpacity="0.05" />
+        </linearGradient>
+        <clipPath id="clip0_179_206">
+          <rect
+            width="24"
+            height="24"
+            fill="white"
+            transform="matrix(1 0 0 -1 10 34)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
 export const ArrowRight = () => {
-    return (
-        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none'
-             stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'
-             className='feather feather-chevron-down'>
-            <polyline points='6 9 12 15 18 9'></polyline>
-        </svg>
-    );
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="feather feather-chevron-down"
+    >
+      <polyline points="6 9 12 15 18 9"></polyline>
+    </svg>
+  );
 };
