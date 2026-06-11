c06a9d0d278fae00869f7c5cc03a6f6418fb1923
Leviachann <amazonsrller@gmail.com>
2026-06-11 12:31:35 +0400

    extra files

--- src/core/shared/slider-container/slider-container.component.tsx
+++ src/core/shared/slider-container/slider-container.component.tsx
@@ -4,7 +4,7 @@ import type { CarouselRef } from 'antd/es/carousel';
 import { useSliderContainerStyles } from './slider-container.style';
 interface SliderContainerProps {
   title: string;
-  children: ReactNode[]; 
+  children: ReactNode[];
 }
 
 const SliderContainer = ({ title, children }: SliderContainerProps) => {
@@ -15,29 +15,35 @@ const SliderContainer = ({ title, children }: SliderContainerProps) => {
     <div className={classes.wrapper}>
       <div className={classes.sectionHeaderWithControls}>
         <h2 className={classes.header}>{title}</h2>
-        
+
         <div className={classes.sliderNavigationBox}>
-          <button className={classes.navButton} onClick={() => carouselRef.current?.prev()}>
+          <button
+            className={classes.navButton}
+            onClick={() => carouselRef.current?.prev()}
+          >
             ‹
           </button>
-          <button className={classes.navButton} onClick={() => carouselRef.current?.next()}>
+          <button
+            className={classes.navButton}
+            onClick={() => carouselRef.current?.next()}
+          >
             ›
           </button>
         </div>
       </div>
-      
+
       <div className={classes.carouselWrapper}>
-<Carousel
-  ref={carouselRef}
-  dots={false}
-  infinite={false} 
-  slidesToShow={3}
-  slidesToScroll={1}
-  responsive={[
-    { breakpoint: 1024, settings: { slidesToShow: 2 } },
-    { breakpoint: 768, settings: { slidesToShow: 1 } }
-  ]}
->
+        <Carousel
+          ref={carouselRef}
+          dots={false}
+          infinite={false}
+          slidesToShow={3}
+          slidesToScroll={1}
+          responsive={[
+            { breakpoint: 1024, settings: { slidesToShow: 2 } },
+            { breakpoint: 768, settings: { slidesToShow: 1 } },
+          ]}
+        >
           {children.map((child, index) => (
             <div key={index} className={classes.carouselItemPadding}>
               {child}
